a4e45257ce4af0453836edad196f55e0196a6e1e
TawheedB <65690320+TawheedB@users.noreply.github.com>
2023-03-22 22:29:02 -0700

    Update fancifymytext.js

--- fancifymytext.js
+++ fancifymytext.js
@@ -1,3 +1,17 @@
 function function_change(){
-  alert("Hello, world!");
+  document.getElementById("TextID").style.fontSize="2em";
+  if(document.getElementById('fancyButton').checked==true){
+
+      document.getElementById("TextID").style.fontWeight="bold";
+      document.getElementById("TextID").style.color="blue";
+      document.getElementById("TextID").style.textDecoration="underline";
+
+  }
+  if(document.getElementById('boringButton').checked==true){
+
+      document.getElementById("TextID").style.fontWeight="normal";
+      document.getElementById("TextID").style.color="black";
+      document.getElementById("TextID").style.textDecoration="none";
+
+  }
 }
