5bb4ab6f93bf57b212b15287d1bc90f482d03dea
TawheedB <65690320+TawheedB@users.noreply.github.com>
2023-03-22 23:09:55 -0700

    Update fancifymytext.js

--- fancifymytext.js
+++ fancifymytext.js
@@ -1,3 +1,12 @@
+function moo()
+{
+  var text=document.getElementById("TextID").value;
+  var uppercase_text=text.toUpperCase();
+  var split_text=uppercase_text.split(".");
+  suffixed_text=split_text.join("-Moo");
+  document.getElementById("TextID").value=suffixed_text;
+
+}
 function function_change(){
   document.getElementById("TextID").style.fontSize="2em";
   if(document.getElementById('fancyButton').checked==true){
@@ -14,13 +23,4 @@ function function_change(){
       document.getElementById("TextID").style.textDecoration="none";
 
   }
-}
-function moo()
-{
-  var text=document.getElementById("TextID").value;
-  var uppercase_text=text.toUpperCase();
-  var split_text=uppercase_text.split(".");
-  suffixed_text=split_text.join("_Moo");
-  document.getElementById("TextID").value=suffixed_text;
-
 }
